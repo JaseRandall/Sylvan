88003f28c346fe9d7d30ea8ed2b06190dda29c4a
Jase <35100435+JaseRandall@users.noreply.github.com>
2026-07-03 18:46:10 +1000

    Guard EncoderStream Close override

---
+++ source/Sylvan.Common.Tests/EncoderStreamApiTests.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace Sylvan.IO;
+
+public sealed class EncoderStreamApiTests
+{
+	[Fact]
+	public void Close_RemainsDeclaredOverride()
+	{
+		var method = typeof(EncoderStream).GetMethod(
+			nameof(Stream.Close),
+			BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+
+		Assert.NotNull(method);
+		Assert.Equal(typeof(Stream), method!.GetBaseDefinition().DeclaringType);
+	}
+}
